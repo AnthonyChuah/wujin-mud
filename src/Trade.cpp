4ebb9f96f7eff0a2ddb570dec6bbcc652731088d
Anthony Chuah <anthony.w.chuah@gmail.com>
2018-12-17 16:38:04 +0800

    Add look, exits
    Minor comments, improve printfs

--- src/Trade.cpp
+++ src/Trade.cpp
@@ -7,7 +7,7 @@
 void Trade::BuySupplies(Character& character, uint16_t qty)
 {
     constexpr uint32_t suppliesCostPerQty = 2;
-    printf("Trade::BuySupplies Character wants to buy %u supplies, and currently has %u/%u, has %u money",
+    printf("Trade::BuySupplies Character wants to buy %u supplies, and currently has %u/%u, has %u money\n",
            qty, character._items.supplies, character._items.maxsupplies, character._items.money);
 
     if (character._items.supplies + qty > character._items.maxsupplies)
@@ -37,7 +37,7 @@ void Trade::BuySupplies(Character& character, uint16_t qty)
 void Trade::BuyAmmo(Character& character, uint16_t qty)
 {
     constexpr uint32_t ammoCostPerQty = 2;
-    printf("Trade::BuyAmmo Character wants to buy %u ammo, and currently has %u/%u, has %u money",
+    printf("Trade::BuyAmmo Character wants to buy %u ammo, and currently has %u/%u, has %u money\n",
            qty, character._items.ammo, character._items.maxammo, character._items.money);
 
     if (character._items.ammo + qty > character._items.maxammo)
